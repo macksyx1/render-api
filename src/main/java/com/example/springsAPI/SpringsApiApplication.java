02de93f6f0778d358aea08639204001d12b8f25e
macksyx1 <markinojales03@gmail.com>
2023-11-24 14:41:16 +0800

    commit all

---
+++ src/main/java/com/example/springsAPI/SpringsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsAPI/SpringsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
